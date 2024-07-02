1174bfb9585c464a6489aab320aca3b781c2d861
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-07-02 23:00:41 +0300

    Добавлена клавиатура для поста приветствия.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types import Message
 from dotenv import load_dotenv
 
@@ -17,27 +18,28 @@ print(TOKEN)
 dp = Dispatcher()
 
 message_text = ("👋 Здравствуй! \n\n"
-                
+
                 "📆 С 12 июля по 4 августа 2024 года мы проводим конкурс. Победители получат возможность тестового "
                 "проживания в нашем доме на территории выставки Open Village24.\n\n"
-                
+
                 "🏡 Для участия в конкурсе достаточно подписаться на официальный telegram-канал компании. Результаты "
                 "будут опубликованы здесь.\n\n"
-                
+
                 "🌞 Прочувствуй загородную жизнь с DRHouse ")
+
+
+def menu_keyboard() -> InlineKeyboardMarkup:
+    """Клавиатура главного меню"""
+    rows = [
+        [InlineKeyboardButton(text='Перейти в канал', url='https://t.me/drhousecosystem')],
+    ]
+    main_menu_key = InlineKeyboardMarkup(inline_keyboard=rows)
+    return main_menu_key
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(message_text)
-
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
+    await message.answer(message_text, reply_markup=menu_keyboard())
 
 
 async def main() -> None:
